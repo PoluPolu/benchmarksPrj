438d32fb84c0087267596a708950b9bc02f78fdd
PoluPolu <ppproko@gmail.com>
2025-01-01 15:23:16 -0500

    Add existing file

---
+++ db/benchmarksPega.sql
@@ -0,0 +1,26 @@
+-- public.benchmarks definition
+
+-- Drop table
+
+-- DROP TABLE public.benchmarks;
+
+CREATE TABLE public.benchmarks (
+	id serial4 NOT NULL,
+	bname text NULL,
+	btype text NULL,
+	kind text NULL,
+	enabled bool DEFAULT false NOT NULL,
+	branches _text NULL,
+	priority int4 DEFAULT 4 NOT NULL,
+	bcycle int4 DEFAULT 12 NOT NULL,
+	boffset int4 DEFAULT 10 NOT NULL,
+	burl text NULL,
+	notification_email text NULL,
+	issues text NULL,
+	notes text NULL,
+	"id""|""bname""|""btype""|""kind""|""enabled""|""branches""|""priority""|""bcy" varchar(256) NULL,
+	CONSTRAINT benchmarks_pkey PRIMARY KEY (id)
+);
+
+truncate benchmarks;
+select * from benchmarks b ;
